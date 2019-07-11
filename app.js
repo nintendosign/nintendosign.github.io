0bf5a82b1d0e062d180656ebdb1bfe6db39893b6
nintendosign <52778385+nintendosign@users.noreply.github.com>
2019-07-11 13:04:20 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
-new Vue({
+let app = new Vue({
     el: '#app',
     data: {
         count: 0,
-        profiles: [],
-        offset: 0
+        offset: 0,
+        profiles: []
     },
     methods: {
-        get () {
-            return new Promise(async (resolve, reject) => {
-                try {
-                    const { data } = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.vk.com/method/wall.getComments?owner_id=-184333261&post_id=2&v=5.52&access_token=c6b36f9cc6b36f9cc6b36f9c18c6d8f3c3cc6b3c6b36f9c9b95b850f5f9982b388a7c71&extended=1&count=100&offset=${this.offset}`);
-                    resolve({ count: data.response.count, users: data.response.profiles });
-                } catch (error) {
-                    reject(error);
-                }
-            })
-        },
-        async parse () {
-            const info = await this.get();
-            for (const user of info.users) {
-                if (user.first_name != 'Администрация ВКонтакте') {
-                    this.profiles.push({ first_name: user.first_name, last_name: user.last_name, photo: user.photo_100 });
-                }
-            }
-            this.offset += info.users.length;
-            this.count = info.count;
-            if (this.offset != this.count) {
-                this.parse()
-            }
+        init ({ response }) {
+            this.count = response.count;
+            this.profiles = this.profiles.concat(response.profiles);
+            this.offset = this.profiles.length;
         }
     },
     mounted: function () {
-        this.parse();
+        setInterval(() => {
+            getComments(`https://api.vk.com/method/wall.getComments?owner_id=-184333261&post_id=2&v=5.52&access_token=c6b36f9cc6b36f9cc6b36f9c18c6d8f3c3cc6b3c6b36f9c9b95b850f5f9982b388a7c71&extended=1&count=100&offset=${this.offset}`, 'app.init');
+        }, 1000);
     }
 })
+
+function getComments (cmd, cb) {
+    let script = document.createElement('SCRIPT');
+    script.src = `${cmd}&callback=${cb}`;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
